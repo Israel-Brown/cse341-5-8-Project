cbc09346ed302089308421890995fa9754d30c81
Israel-Brown <menelek7171@byui,edu>
2025-01-18 17:45:36 -0500

    Final1.1

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const contactsRoute = require('./routes/contacts');
 
 // Home route
 app.get('/', (req, res) => {
     res.send('Israel Brown');
 });
+
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+    const contactsRoute = require('./routes/contacts');
+    app.use('api/contacts', contactsRoute);
 
 // Start the server
 app.listen(PORT, () => {
