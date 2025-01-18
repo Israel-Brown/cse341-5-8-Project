7180a1870c547bef0a66da0e3d2783478c25588f
Israel-Brown <menelek7171@byui,edu>
2025-01-18 16:40:07 -0500

    Finalfinal

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const app = express();
-const homeRoute = require('./routes/home');
-const controllers = require('./controllers/homeController');
-
-
 const PORT = process.env.PORT || 3000;
 
-// Use the home route
-app.use('/', homeRoute);
+// Home route
+app.get('/', (req, res) => {
+    res.send('Israel Brown');
+});
 
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
-
