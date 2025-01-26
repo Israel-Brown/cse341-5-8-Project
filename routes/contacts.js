47e2273fa371b6939dc8907395401691fc226dcd
Israel-Brown <menelek7171@byui,edu>
2025-01-26 01:25:08 -0500

    fet

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Contact = require('./routes/index');
+const Contact = require('../controllers/contact')
 
 // GET all contacts
 router.get('/', async (req, res) => {
