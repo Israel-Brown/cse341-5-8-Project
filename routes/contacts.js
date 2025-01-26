d0965e32ee375c6d49f4b367d5dc5ae920b1d1b4
Israel-Brown <menelek7171@byui,edu>
2025-01-26 01:19:02 -0500

    get

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Contact = require('models/contact');
+const Contact = require('./routes/index');
 
 // GET all contacts
 router.get('/', async (req, res) => {
