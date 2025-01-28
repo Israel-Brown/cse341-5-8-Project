e30f3291bebe6c0b8cf54a80aa64ba96f1f49c9b
Israel-Brown <menelek7171@byui.edu>
2025-01-28 16:59:40 -0500

    nir

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express'); // Import swagger-ui-express
 const swaggerDocument = require('./swagger.json'); // Import the generated swagger.json
-const mongodb = require('./db/connect');
+const mongoose = require('mongoose');
+const http = require('http');
+const dotenv = require('dotenv'); dotenv.config();
 const app = express();
-
-const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json())
    .use((req, res, next) => {
@@ -15,12 +15,27 @@ app.use(bodyParser.json())
    .use('/', require('./routes'))
    .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); // Serve Swagger UI
 
-mongodb.initDb((err) => {
-   if (err) {
-      console.log(err);
-   } else {
-      app.listen(port, () => {
-         console.log(`Server is running on port ${port}`);
+   const connectDB = async () => {
+      try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        });
+        console.log('Connected to database!');
+      } catch (err) {
+        console.error('Connection failed:', err);
+        process.exit(1); // Exit process if connection fails
+      }
+    };
+    
+    connectDB().then(() => {
+      const port = process.env.PORT || '3000';
+      app.set('port', port);
+    
+      const server = http.createServer(app);
+    
+      server.listen(port, () => {
+        console.log(`API running on localhost:${port}`);
       });
-   }
-});
+    });
+    
