cd6309b615367eea944b0a77a77daa3918748971
Israel-Brown <menelek7171@byui.edu>
2025-01-28 07:40:32 -0500

    send

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,20 @@
-const getName = (req, res) => {
-  res.send('Israel Brown');
+const mongodb = require('../db/connect');
+
+const getUser = async (req, res, next) => {
+  const result = await mongodb.getDb().db().collection('user').find();
+  result.toArray().then((lists) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists[0]);
+  });
 };
 
-module.exports = { getName };
+const getUsername = async (req, res, next) => {
+  const result = await mongodb.getDb().db().collection('user').find();
+  result.toArray().then((lists) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists[0].username);
+  });
+};
+
+module.exports = { getUser, getUsername };
 
